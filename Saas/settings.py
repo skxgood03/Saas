5cb6620908334281e49b9fd8827fc92861fa3c88
shikexuan <2399745557@qq.com>
2021-05-26 17:55:50 +0800

    问题操作页面实现滚动栏效果

--- Saas/settings.py
+++ Saas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app01',
-    'web'
+    'web',
+
 ]
 
 MIDDLEWARE = [
@@ -154,7 +155,7 @@ WHITE_REGEX_URL_LIST = [
     '/send/sms/',
     '/login/',
     '/image_code/',
-    '',
+    '/',
     '/login/sms/',
     '/price/',
 
